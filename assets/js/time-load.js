774a14ee821493fb426b6fc1885327a482fe6711
Lopa10ko <justforprojects@mail.ru>
2024-08-23 16:39:41 +0300

    feat: add 4-digit precision for a load time

--- assets/js/time-load.js
+++ assets/js/time-load.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loadTimeElement = document.getElementById('load-time');
     const loadYearElement = document.getElementById('load-year');
-    loadTimeElement.innerHTML = performance.now() / 1000  + "s";
+    loadTimeElement.innerHTML = (performance.now() / 1000).toPrecision(4)  + "s";
     loadYearElement.innerHTML = new Date().getFullYear().toString();
 });
